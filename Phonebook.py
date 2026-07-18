2594713ea3f2a0b3b0a6bba17f2f76a4eccef686
Vipul pandey <vipul.pandey884@gmail.com>
2026-07-18 17:49:31 +0530

    Initial Commit

---
+++ Phonebook.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+
+
+
+
+
+def add_contact():
+    pass
+
+def add_multiple_contacts():
+    pass
+
+def search_contact():
+    pass
+
+def edit_contact():
+    pass
+
+def delete_contact():
+    pass
+
+def delete_all():
+    pass
+
+def main():
+    while True:
+        print("----------------------------")
+        print("_____Phonebook__Manager_____")
+        print("----------------------------")
+        print("1. Add Contact")
+        print("2. Add Multiple Contacts")
+        print("3. Search Contact")
+        print("4. Edit Contact")
+        print("5. Delete Contact")
+        print("6. Delete all")
+        print("7. Exit")
+        choice = input("Select Options.. ")
+        match choice:
+            case "1":
+                add_contact()
+            case "2":
+                add_multiple_contacts()
+            case "3":
+                search_contact()
+            case "4":
+                edit_contact()
+            case "5":
+                delete_contact()
+            case "6":
+                delete_all()
+            case "7":
+                break
+            case _:
+                print("Invalid Choice..")
+        
+
+
+if __name__=="__main__":
+    main()
